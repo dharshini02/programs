7d130ee3342e3517454e81e185ca2b16e8abfdf6
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-08 22:01:17 +0530

    Create VowlesOrConsonant.java

---
+++ VowlesOrConsonant.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args)throws Exception {
+           Scanner sc=new Scanner(System.in);
+           String ch=sc.nextLine();
+          if(ch=="a"|| ch=="e"||ch=="i"||ch=="o"||ch=="u")
+               System.out.println("vowles");
+           else 
+             System.out.println("consonant");
+    }
+}
