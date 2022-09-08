abb965520e2f62071d0e99744beb4255e101d436
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-08 23:21:50 +0530

    Create Day3 FindAlphabets.java

---
+++ Day3 FindAlphabets.java
@@ -0,0 +1,10 @@
+class Alphabetic {
+public static void main(String ar[]) throws Exception
+{
+char ch="a";
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+ System.out.println("It is an alphabet");
+else
+ System.out.print("It's not an alphabet");
+}
+}
