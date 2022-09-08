51b3c152591a8ad8629cfbbc3e8ec8a65afe6f41
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-08 21:01:01 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Swap
+{
+    public static void main(String args[])throws IOException
+    {
+        DataInputStream d=new DataInputStream(System.in);
+        System.out.print("Enter the value=");
+        int a=Integer.parseInt(d.readLine());
+        System.out.print("Enter the value=");
+        int b=Integer.parseInt(d.readLine());
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("swap values are is="+a,b);
+    }
+}
