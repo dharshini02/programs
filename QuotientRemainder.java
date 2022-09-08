b3348c8db4dcdfdf213b7de855d50afb4cc88263
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-08 20:42:45 +0530

    Create QuotientRemainder.java

---
+++ QuotientRemainder.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class quotientRemainder
+{
+    public static void main(String args[])throws IOException
+    {
+        DataInputStream d=new DataInputStream(System.in);
+        System.out.print("Enter the dividend=");
+        int n=Integer.parseInt(d.readLine());
+        System.out.print("Enter the divisor=");
+        int div=Integer.parseInt(d.readLine());
+        int qu=n/div;
+        int remain=n%div;
+        System.out.println("The Quotient is="+qu);
+        System.out.println("The Remainder is="+remain);
+    }
+}
