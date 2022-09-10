16411a98598a0c424157b915a6664bd6ea9cfae8
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-10 23:17:12 +0530

    Create Day 4 Sum of natural num.java

---
+++ Day 4 Sum of natural num.java
@@ -0,0 +1,10 @@
+class natural{
+  public staic void main(String ar[]) throws Exception
+  {
+     int n=10,s=0;
+    for(int i=1;i<=n;i++)
+    {
+      s=s+i;
+    }
+    Sytem.out.println("The  natural number is "+s);
+  }
