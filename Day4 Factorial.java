7e12c7afcc7fe351192d5b102ea72be182c12ab1
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-10 23:19:28 +0530

    Create Day4 Factorial.java

---
+++ Day4 Factorial.java
@@ -0,0 +1,10 @@
+class Fact{
+  public staic void main(String ar[]) throws Exception
+  {
+     int n=10,s=1;
+    for(int i=1;i<=n;i++)
+    {
+      s=s*i;
+    }
+    Sytem.out.println("The  natural number is "+s);
+  }
