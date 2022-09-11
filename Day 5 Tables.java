1a66f8c10c2cb06bec35245f2722e2c1b7cf74bc
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-11 22:19:06 +0530

    Create Day 5 Tables.java

---
+++ Day 5 Tables.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class table{
+   public static void main(String ar[])throws Exception
+   {
+    int a=0,m,sum;
+    int n=10;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value:");
+     c=sc.nextInt();
+    for(int i=1;i<=n;i++)
+    {
+        a=c+a;
+        System.out.println(c+"*"+i+"="+a);
+    }
+   }
+}
