d5fd02682430c59fea9bc67318cc4f5112a4d417
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-18 18:45:02 +0530

    Update Gcd.java

--- Gcd.java
+++ Gcd.java
@@ -1,14 +1,14 @@
 public class Gcd{
- static int recursiongcd(int a,int b);
+public static int recursiongcd(int a,int b);
  {
    if(b!=0)
      return recursiongcd(b,a%b);
    else
-     return a;
+     return ;
   }
 public static void main(String []ar)throws Exception
 {
  int n1=32,n2=7;
- System.out.println(recursiongcd(n1,n2);
+ System.out.println(recursiongcd(n1,n2));
 }
 }
