962022de73ea5516aed82fa3e0b26b7651678b7b
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-18 18:24:36 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,12 @@
+public class Gcd{
+ static int recursiongcd(int a,int b);
+ {
+   if(b!=0)
+     return recursiongcd(b,a%b);
+  }
+public static void main(String []ar)throws Exception
+{
+ int n1=32,n2=7;
+ System.out.println(recursiongcd(n1,n2);
+}
+}
