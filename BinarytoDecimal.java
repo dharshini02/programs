3d923996b033408bc3a0c933ad801538550c8930
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-18 19:23:50 +0530

    Create BinarytoDecimal.java

---
+++ BinarytoDecimal.java
@@ -0,0 +1,22 @@
+public class BinarytoDecimal
+{
+ public static int btod(int n)
+ {
+  int num=n;
+ int dec=0,base=1;
+  int temp=num;
+   while(temp>0)
+   {
+    int l=temp%10;
+    temp=temp/10;
+   dec=dec+l*base;
+   base=base*2;
+}
+return dec;
+}
+public static void main(String ar[])throws Exception
+{
+  int number=1011;
+  System.out.println(btod(number));
+}
+}
