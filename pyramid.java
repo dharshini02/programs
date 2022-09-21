fa0bf02a5dea7fd658b79f778cdff4534d245c24
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-21 20:06:21 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,15 @@
+public class pyramid
+{
+ public static void main(String[] args)
+  {
+    int t= 4;
+    for (int i = 1; i <= r; i++)
+       {
+      for (int j = 1; j <= i; j++) 
+       {
+        System.out.print("* ");
+       }
+      System.out.println();
+    }
+  }
+}
