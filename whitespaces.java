d9210c480e704f8b0980c1b4179134cc59d69919
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-22 18:57:39 +0530

    Create whitespaces.java

---
+++ whitespaces.java
@@ -0,0 +1,9 @@
+class whitespaces{
+ public static void main(String ar[])
+ {
+  String Str="Hell o worl d";
+  String st=Str.replaceAll("\\s","");
+ System.out.println("Before removal:"+Str);
+  System.out.println("After removal:"+st);
+}
+}
