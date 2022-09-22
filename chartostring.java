cb232ece119e6f532447caba84f259d8741409bd
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-22 18:06:05 +0530

    Create chartostring.java

---
+++ chartostring.java
@@ -0,0 +1,17 @@
+class chartostring{
+ public static void main(String[]ar){
+  char c[]={'a','e','i','o','u'};
+  String str[]=String.valueOf(c);
+   System. out.println(str);
+}
+}
+
+
+
+class chartostring{
+ public static void main(String[]ar){
+  char c[]={'a','e','i','o','u};
+  String str[]=c.toString();
+  System. out.println(str);
+}
+}
