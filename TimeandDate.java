ab7f9b81ba7f142087b9bed2d2d4bb896e2b4ee1
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-24 18:06:41 +0530

    Create TimeandDate.java

---
+++ TimeandDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+public class TimeandDate{
+ public static void main(String ar[])throws Exception
+  {
+   LocalDateTime cur=LocalDateTime.now();
+   System.out.println("Current date and time:"+cur);
+}
+}
