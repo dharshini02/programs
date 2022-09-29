fbc8f50d239ded26fd63c9f52561f5c9474ce3fe
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-29 19:34:13 +0530

    Create settoarray.java

---
+++ settoarray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class settoarray {
+    public static void main(String[] args) {
+      String ar[]={"a","b","c","d"};
+      Set<String>str=new HashSet<>(Arrays.asList(ar));
+        System.out.println("the set:"+str);
+ 
+    }
+}
