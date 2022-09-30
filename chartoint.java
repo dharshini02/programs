90749cba75aced23c201f0e65c72679d8c2d6fba
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-30 20:34:13 +0530

    Create chartoint.java

---
+++ chartoint.java
@@ -0,0 +1,10 @@
+class chartoint {
+  public static void main(String[] args) {
+    char a = '9';
+    char b = '3';
+    int n1 = a - '0';
+    int n2 = b - '0';
+    System.out.println(n1);    
+    System.out.println(n2);    
+  }
+}
