f7e200bddda46af15cabfdda4322f229192f5531
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-30 20:29:27 +0530

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,10 @@
+class numeric{
+String string = "100";
+
+if(isNumeric(string)) { 
+    System.out.println("String is numeric!");
+    
+} else {
+    System.out.println("String is not numeric.");
+}
+}
