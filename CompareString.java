52b2ad38dd859234c610d775804234c05537a4ff
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-09-30 20:25:32 +0530

    Create CompareString.java

---
+++ CompareString.java
@@ -0,0 +1,13 @@
+public class CompareString {
+
+    public static void main(String[] args) {
+
+        String s1 = new String("Hello");
+        String s2 = new String("Hello");
+
+        if(s1.equals(s2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
