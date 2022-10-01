217cb4e2d10138c6f834c6f8e651dc50cd566ba9
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-10-01 18:21:46 +0530

    Create inttochar.java

---
+++ inttochar.java
@@ -0,0 +1,10 @@
+class inttochar {
+  public static void main(String[] ar){
+    int num1 = 3;
+    int num2 = 2;
+    char a = (char)(num1 + '0');
+    char b = (char)(num2 + '0');
+    System.out.println(a);   
+    System.out.println(b);    
+  }
+}
