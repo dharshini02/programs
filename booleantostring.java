36980a0691a179d0b380f3892df4e6b9bd159ad8
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-10-01 18:31:50 +0530

    Create booleantostring.java

---
+++ booleantostring.java
@@ -0,0 +1,10 @@
+class booleantostring {
+  public static void main(String[] args) {
+    boolean Value1 = true;
+    boolean Value2 = false;
+    String string1 = Boolean.toString(Value1);
+    String string2 = Boolean.toString(Value2);
+    System.out.println(string1);  
+    System.out.println(string2);   
+  }
+}
