9e32b215efbb1bc68e5a5973c5d491f05537fe6b
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-10-05 20:24:27 +0530

    Create Candies.java

---
+++ Candies.java
@@ -0,0 +1,11 @@
+class Candies
+{
+  public static void main(String[] args)throws IOException 
+  {
+    int m=10,n=5;
+    if (m%n==0)
+    System.out.println("YES");
+    else 
+     System.out.println("NO");
+  }
+}
