181af785a111eed8405ffbb6d6f313f3a0461ff1
Dharshini S <99235220+dharshini02@users.noreply.github.com>
2022-10-08 18:13:35 +0530

    Create SwapStrings.java

---
+++ SwapStrings.java
@@ -0,0 +1,11 @@
+public class SwapStrings     
+{    
+    public static void main(String[] args) {    
+        String str1 = "Hello", str2 = "world ";         
+        str1 = str1 + str2;      
+        str2 = str1.substring(0, (str1.length() - str2.length()));       
+        str1 = str1.substring(str2.length());    
+            
+        System.out.println("Strings swapping: " + str1 + " " + str2);    
+    }    
+}    
